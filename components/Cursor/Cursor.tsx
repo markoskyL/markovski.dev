6e8611bd753c8f705c8294f571d3a758799e0175
Leonardo Markovski <leonardo.markosky@gmail.com>
2022-10-04 22:37:13 +0200

    cursor: fixed bug

--- components/Cursor/Cursor.tsx
+++ components/Cursor/Cursor.tsx
@@ -1,20 +1,8 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./Cursor.module.scss";
 const Cursor = () => {
   const innerCursor = useRef<HTMLDivElement>(null);
   const outerCursor = useRef<HTMLDivElement>(null);
-  const links = Array.from(document.querySelectorAll("a"));
-
-  links.forEach((link) => {
-    link.addEventListener("mouseover", () => {
-      innerCursor.current?.classList.add("grow");
-      outerCursor.current?.classList.add("grow");
-    });
-    link.addEventListener("mouseleave", () => {
-      innerCursor.current?.classList.remove("grow");
-      outerCursor.current?.classList.remove("grow");
-    });
-  });
 
   const moveCursor = (e: MouseEvent) => {
     let x = e.clientX;
@@ -25,8 +13,23 @@ const Cursor = () => {
     outerCursor.current.style.left = `${x}px`;
     outerCursor.current.style.top = `${y}px`;
   };
+  
+  useEffect(() => {
+    const links = Array.from(document.querySelectorAll("a"));
 
-  document.addEventListener("mousemove", moveCursor);
+    links?.forEach((link) => {
+      link.addEventListener("mouseover", () => {
+        innerCursor.current?.classList.add("grow");
+        outerCursor.current?.classList.add("grow");
+      });
+      link.addEventListener("mouseleave", () => {
+        innerCursor.current?.classList.remove("grow");
+        outerCursor.current?.classList.remove("grow");
+      });
+    });
+
+    document.addEventListener("mousemove", moveCursor);
+  }, []);
 
   return (
     <>
