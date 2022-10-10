68ac8f83ad6c9ab33752ced96cea6402905c0860
Leonardo Markovski <leonardo.markosky@gmail.com>
2022-10-11 00:48:07 +0200

    Update ProjectsPage.tsx

--- components/_ProjectsPage/ProjectsPage.tsx
+++ components/_ProjectsPage/ProjectsPage.tsx
@@ -7,7 +7,9 @@ const ProjectsPage = () => {
     <div className={styles.projects} id="projects">
       <div className={styles.projectsInner}>
         <h1 className={styles.pageTitle}>projects.</h1>
-        <h2 className={styles.pagePretitle}>A selection of stuff i've built</h2>
+        <h2 className={styles.pagePretitle}>
+          A selection of stuff i&apos;ve built
+        </h2>
         <div className={styles.projectsCardsContainer}>
           {data.map((project) => (
             <ProjectCard {...project} key={project.id} />
